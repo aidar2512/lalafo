2e0fb212f01c160110cc2fe727a1f00b3792cae2
Aidar Ormokoev <gumasroid@gmail.com>
2025-10-17 23:08:55 +0600

    application structure

---
+++ src/main/java/kg/mega/lalafo/LalafoApplication.java
@@ -0,0 +1,13 @@
+package kg.mega.lalafo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalafoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LalafoApplication.class, args);
+    }
+
+}
